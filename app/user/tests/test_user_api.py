1e33c641e992fcd67f622e2cfa7c380adbd4c55b
Vierageorge <joviera@gmail.com>
2020-08-12 23:04:26 -0500

    Fixes test on test_user_api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -36,7 +36,8 @@ class PublicUserApiTests(TestCase):
         """Test creating a user that already exists fails"""
         payload = {
             'email': 'test@test.com',
-            'password': 'password123'
+            'password': 'password123',
+            'name': 'Test Name'
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
@@ -46,7 +47,8 @@ class PublicUserApiTests(TestCase):
         """Test that the password must be more than 5 characters"""
         payload = {
             'email': 'test@test.com',
-            'password': 'pwd'
+            'password': 'pwd',
+            'name': 'Test Name'
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
